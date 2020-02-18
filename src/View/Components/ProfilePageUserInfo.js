5a11ce426d37d940fcb745fd1ba3e8de948ac193
Abel Miri <miri.1888@gmail.com>
2020-02-18 18:16:04 +0330

    password change added

--- src/View/Components/ProfilePageUserInfo.js
+++ src/View/Components/ProfilePageUserInfo.js
@@ -15,8 +15,12 @@ class ProfilePageUserInfo extends Component
             entrance: "",
             birth_date: "",
             university: "",
+            password: "",
             loading: false,
             done: false,
+            pass_modal: false,
+            done_pass: false,
+            wrong_pass: false,
         }
     }
 
@@ -34,7 +38,7 @@ class ProfilePageUserInfo extends Component
                 entrance: user.entrance && user.entrance,
                 major: user.major && user.major,
                 grade: user.grade && user.grade,
-                // password: user.password && user.password,
+                password: user.password && user.password,
             })
         }
     }
@@ -92,32 +96,47 @@ class ProfilePageUserInfo extends Component
         })
     }
 
-    // handlePassChange = () =>
-    // {
-    //     const {email, name, major, grade, entrance, birth_date, university} = this.state
-    //     const {setUser} = this.props
-    //
-    //     this.setState({...this.state, loading: true, done: false}, () =>
-    //     {
-    //         api.patch("user", {email, name, major, grade, entrance, birth_date, university}, "")
-    //             .then((res) =>
-    //             {
-    //                 this.setState({...this.state, loading: false, done: true}, () => setUser(res))
-    //             })
-    //             .catch((e) =>
-    //             {
-    //                 if (e.message === "Request failed with status code 404")
-    //                 {
-    //                     localStorage.clear()
-    //                     this.setState({...this.state, loading: false})
-    //                 }
-    //             })
-    //     })
-    // }
+    handlePassModal = () =>
+    {
+        const {pass_modal, loading} = this.state
+        if (!loading)
+        {
+            this.setState({...this.state, pass_modal: !pass_modal, done_pass: false, wrong_pass: false})
+        }
+    }
+
+    handlePassChange = () =>
+    {
+        const {password} = this.state
+        const {setUser} = this.props
+        const oldPass = this.oldPasswordInput.value
+        const newPass = this.newPasswordInput.value
+
+        if (oldPass === password && newPass.length >= 8 && newPass.length <= 20)
+        {
+            this.setState({...this.state, loading: true, done_pass: false, wrong_pass: false}, () =>
+            {
+                api.patch("user", {password: newPass}, "")
+                    .then((res) =>
+                    {
+                        this.setState({...this.state, loading: false, done_pass: true, password: newPass}, () => setUser(res))
+                    })
+                    .catch((e) =>
+                    {
+                        if (e.message === "Request failed with status code 404")
+                        {
+                            localStorage.clear()
+                            this.setState({...this.state, loading: false})
+                        }
+                    })
+            })
+        }
+        else this.setState({...this.state, wrong_pass: true})
+    }
 
     render()
     {
-        const {email, name, major, grade, entrance, birth_date, university, loading, done} = this.state
+        const {email, name, major, grade, entrance, birth_date, university, pass_modal, loading, done, done_pass, wrong_pass} = this.state
         return (
             <div className="profile-introduction">
                 <div className="profile-introduction-content">
@@ -128,7 +147,7 @@ class ProfilePageUserInfo extends Component
                         <p>نام کامل</p>
                         <input type="text" placeholder="نام و نام خانوادگی" defaultValue={name ? name : ""} onBlur={(e) => this.setUserData(e, "name")}/>
                         <p>ایمیل</p>
-                        <input type="text" placeholder="name@provider.dom" defaultValue={email ? email : ""} dir="ltr" onBlur={(e) => this.setUserData(e, "email")}/>
+                        <input type="email" placeholder="name@provider.dom" defaultValue={email ? email : ""} dir="ltr" onBlur={(e) => this.setUserData(e, "email")}/>
                         <p>تاریخ تولد</p>
                         <input type="text" placeholder="1375/05/15" defaultValue={birth_date ? birth_date : ""} onBlur={(e) => this.setUserData(e, "birth_date")}/>
                         <p>نام دانشگاه</p>
@@ -145,10 +164,47 @@ class ProfilePageUserInfo extends Component
                             <Material type='button' onClick={!loading ? this.handleSubmit : null} className={loading ? "profile-info-submit-button loading" : "profile-info-submit-button"}>
                                 ثبت
                             </Material>
-                            <Material type='button' onClick={!loading ? this.handleSubmit : null} className={loading ? "profile-info-submit-button loading" : "profile-info-submit-button"}>
+                            <Material type='button' onClick={!loading ? this.handlePassModal : null} className={loading ? "profile-info-submit-button loading" : "profile-info-submit-button"}>
                                 تغییر رمز
                             </Material>
                         </div>
+                        {pass_modal ?
+                            <React.Fragment>
+                                <div className={"create-exchange-cont create-small"}>
+                                    <div className='create-exchange-title'>تغییر رمز پروفایل</div>
+                                    <div className="create-exchange-main">
+                                        <div className="create-exchange-part">
+                                            <div className='create-exchange-section'>
+                                                <label className='create-exchange-section-label'>رمز قدیمی <span>*</span></label>
+                                                <input type='password'
+                                                       className='create-exchange-section-input'
+                                                       placeholder="رمز پروفایل"
+                                                       maxLength={20}
+                                                       ref={e => this.oldPasswordInput = e}
+                                                />
+                                            </div>
+                                            <div className='create-exchange-section'>
+                                                <label className='create-exchange-section-label'>رمز جدید <span>*</span></label>
+                                                <input type='password'
+                                                       className='create-exchange-section-input'
+                                                       placeholder="بین 8 تا 20 کاراکتر"
+                                                       maxLength={20}
+                                                       ref={e => this.newPasswordInput = e}
+                                                />
+                                            </div>
+                                            {done_pass && <p className="profile-info-submit-success-text">رمز با موفقیت تغییر کرد.</p>}
+                                            {wrong_pass && <p className="profile-info-submit-error-text">رمز غلط است.</p>}
+                                        </div>
+                                        <div>
+                                            <Material className={loading ? "create-exchange-submit loading" : "create-exchange-submit"} onClick={!loading ? this.handlePassChange : null}>
+                                                تغییر رمز
+                                            </Material>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="create-exchange-back" onClick={!loading ? this.handlePassModal : null}/>
+                            </React.Fragment>
+                            : null}
                     </div>
                 </div>
             </div>
