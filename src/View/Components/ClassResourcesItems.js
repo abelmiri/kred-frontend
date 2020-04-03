4d521d81df4c3cc4a3daa8fc8cf3cdf53bcf5574
Abel Miri <miri.1888@gmail.com>
2020-04-04 02:36:48 +0430

    view

--- src/View/Components/ClassResourcesItems.js
+++ src/View/Components/ClassResourcesItems.js
@@ -1,28 +1,31 @@
 import React from "react"
 import LikeSvg from "../../Media/Svgs/LikeSvg"
 import {Link} from "react-router-dom"
+import CommentSvg from "../../Media/Svgs/CommentSvg"
 
 export function ClassResourcesItems(props)
 {
     const {item, svg} = props
     return (
         <Link to={`resources/${item._id}`} className="class-handout-item">
-            <div className="class-handout-item-detail">
-                <LikeSvg className="class-handout-item-detail-like-svg"/>
-                {item.likes_count}
-            </div>
-            <div className="class-handout-item-blur-container">
-                {svg}
-                <div className="class-handout-item-title-cont">
-                    <div className="class-handout-item-title">
-                        {item.title}
-                    </div>
-                    <div className="class-handout-item-description">
-                        {item.university}{item.teacher && ` - ${item.teacher}`}
-                    </div>
-                    <div className="class-handout-item-description">
-                        {item.pages_count} صفحه
-                    </div>
+            {svg}
+            <div className="class-handout-item-title-cont">
+                <div className="class-handout-item-title">
+                    {item.title}
+                </div>
+                <div className="class-handout-item-description">
+                    {item.university}{item.teacher && ` - ${item.teacher}`}
+                </div>
+                <div className="class-handout-item-description">
+                    {item.pages_count} صفحه
+                </div>
+                <div className="class-handout-item-like">
+                    <LikeSvg className="class-handout-item-detail-like-svg"/>
+                    {item.likes_count}
+                </div>
+                <div className="class-handout-item-comment">
+                    {item.comments_count ? item.comments_count : 0}
+                    <CommentSvg className="class-handout-item-detail-comment-svg"/>
                 </div>
             </div>
         </Link>
