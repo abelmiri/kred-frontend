f665c495f64212ee8972b9f1001b1af3d6d45c42
Hoseyn Mousavi <hoseyn.mousavi78@gmail.com>
2020-07-14 13:08:19 +0430

    code verification limit extended

--- src/View/Pages/SignUpPage.js
+++ src/View/Pages/SignUpPage.js
@@ -253,6 +253,10 @@ class SignUpPage extends PureComponent
                                     .catch(() => this.setState({...this.state, loading: false}, () => NotificationManager.error("سیستم با خطا مواجه شد! اینترنت خود را بررسی کنید!")))
                             }
                             else if (e?.response?.status === 403) NotificationManager.error("شما صلاحیت کافی برای ثبت نام ندارید.")
+                            else if (e?.response?.status === 400 && e?.response.data.message === "you get it many time!")
+                            {
+                                this.setState({...this.state, level: 2, loading: false}, () => NotificationManager.warning("کد 3 بار برای شما فرستاده شده است!"))
+                            }
                             else NotificationManager.error("سیستم با خطا مواجه شد! اینترنت خود را بررسی کنید!")
                         }))
                 }
